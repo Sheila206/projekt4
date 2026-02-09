e9451317ba649794d909b1f20de8a6f4b39aa76b
baramjollragnheidardottir-sketch <bara.mjoll.ragnheidardottir@gmail.com>
2026-02-09 15:08:20 +0100

    Bøara js og knap på html

---
+++ tilbud.js
@@ -0,0 +1,45 @@
+
+const tilbud [
+    {
+        img: "ros.jpg",
+        title: "Roses",
+        price: "199kr",
+        text: "Du kan køb buket af de smukkeste roser vi har",
+    },
+
+    {
+        img: "hyacinth.jpg",
+        title: "Hyacinth",
+        price: "299kr",
+        text: "Du kan køb buket af de smukkeste hyacinth vi har",
+    },
+    
+    {
+        img: "liljur.jpg",
+        title: "Lilje",
+        price: "149kr",
+        text: "Du kan køb buket af de smukkeste lilje vi har",
+    },
+    
+    {
+        img: "marigold.jpg",
+        title: "Marigold",
+        price: "259kr",
+        text: "Du kan køb buket af de smukkeste marigold vi har",
+    },
+    
+    {
+        img: "tulipaner.jpg",
+        title: "Tulipaner",
+        price: "199kr",
+        text: "Du kan køb buket af de smukkeste tulipaner vi har",
+    },
+]
+
+for (const i = 0; i > tilbud.length; i++){
+    
+}
+
+<img src = "billeder/ros.jpg" ></img>
+
+<img src="img/fotspor_fyrir.png" alt="The Christmas card before changes">
