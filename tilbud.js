87b21d14b13c5113b8922267b61f1dd5f5ea4f37
baramjollragnheidardottir-sketch <bara.mjoll.ragnheidardottir@gmail.com>
2026-02-10 18:52:37 +0100

    dgdg

--- tilbud.js
+++ tilbud.js
@@ -1,50 +1,42 @@
 function button() {
-  const tilbud [
+  let tilbud = [
     {
-        img: "ros.jpg",
         title: "Roses",
         price: "199kr",
-        text: "Du kan køb buket af de smukkeste roser vi har",
+        text: "Du kan køb buket af de smukkeste roser vi har"
     },
 
     {
-        img: "hyacinth.jpg",
         title: "Hyacinth",
         price: "299kr",
-        text: "Du kan køb buket af de smukkeste hyacinth vi har",
+        text: "Du kan køb buket af de smukkeste hyacinth vi har"
     },
     
     {
-        img: "liljur.jpg",
         title: "Lilje",
         price: "149kr",
-        text: "Du kan køb buket af de smukkeste lilje vi har",
+        text: "Du kan køb buket af de smukkeste lilje vi har"
     },
     
     {
-        img: "marigold.jpg",
         title: "Marigold",
         price: "259kr",
-        text: "Du kan køb buket af de smukkeste marigold vi har",
+        text: "Du kan køb buket af de smukkeste marigold vi har"
     },
     
     {
-        img: "tulipaner.jpg",
         title: "Tulipaner",
         price: "199kr",
-        text: "Du kan køb buket af de smukkeste tulipaner vi har",
+        text: "Du kan køb buket af de smukkeste tulipaner vi har"
     },
   ]
+for (let i = 0; i < tilbud.length; i++){
+ alert("halloooooo")
+ }
 }
-
 // manglar funtion, indhold skal være alert 
 // og en id som skal indhold array i, så skal jeg set disse function i button på html
 
-
-
-for (const i = 0; i > tilbud.length; i++){
-
-}
 
 <img src = "billeder/ros.jpg" ></img>
 
