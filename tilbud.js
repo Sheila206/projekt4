6c3fdd927b2930f9c7fb241f2015374c5e46537e
baramjollragnheidardottir-sketch <bara.mjoll.ragnheidardottir@gmail.com>
2026-02-10 22:13:41 +0100

    ss

--- tilbud.js
+++ tilbud.js
@@ -1,49 +1,52 @@
 function button() {
   let tilbud = [
     {
-        title: "Roses",
+        title: "Mandags tilbud",
         price: "199kr",
         text: "Du kan køb buket af de smukkeste roser vi har",
         image: "billeder/ros.jpg"
     },
 
     {
-        title: "Hyacinth",
+        title: "Tirsdags tilbud",
         price: "299kr",
         text: "Du kan køb buket af de smukkeste hyacinth vi har",
         image: "billeder/hyacinth.jpg"
     },
     
     {
-        title: "Lilje",
+        title: "Onsdags tilbud",
         price: "149kr",
         text: "Du kan køb buket af de smukkeste lilje vi har",
         image: "billeder/liljur.jpg"
     },
     
     {
-        title: "Marigold",
+        title: "torsdags tilbud",
         price: "259kr",
         text: "Du kan køb buket af de smukkeste marigold vi har",
         image: "billeder/marigold.jpg"
     },
     
     {
-        title: "Tulipaner",
+        title: "Fredags tilbud",
         price: "199kr",
         text: "Du kan køb buket af de smukkeste tulipaner vi har",
         image: "billeder/tulipaner.jpg"
     },
-  ]
+ ];
 
-  let alletilbud = "";
+  let container = document.getElementById("tilbudContainer");
+  container.innerHTML = "";
 
-for (let i = 0; i < tilbud.length; i++) {
-    alletilbud = alletilbud
-      + (i + 1) + ". " + tilbud[i].title + "\n"
-      + "Pris: " + tilbud[i].price + "\n"
-      + tilbud[i].text + "\n\n";
- }
- alert(alletilbud);
+  for (let i = 0; i < tilbud.length; i++) {
+    container.innerHTML = container.innerHTML
+      + "<div>"
+      + "<img src='" + tilbud[i].image + "' alt='" + tilbud[i].title + "' width='200'>"
+      + "<h3>" + tilbud[i].title + "</h3>"
+      + "<p>Pris: " + tilbud[i].price + "</p>"
+      + "<p>" + tilbud[i].text + "</p>"
+      + "</div>";
+  }
 }
 
